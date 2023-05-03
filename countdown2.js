0a29ecdbc230035374583e8642f6bfb6cf3c24df
avligation <132170390+avligation@users.noreply.github.com>
2023-05-04 01:41:47 +0300

    Updates
    Αλλαξα την μεταβλητή countdownDate σε countdownDate1 για να μην είναι ίδια και στα δυο αρχεία js, δημιουργούσε προβλήματα

--- countdown2.js
+++ countdown2.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate1 = new Date("May 4, 2023 20:00:00").getTime();
+var countDownDate2 = new Date("May 5, 2023 14:00:00").getTime();
         
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -8,7 +8,7 @@ var x = setInterval(function() {
   var now = new Date().getTime();
     
   // Find the distance between now and the count down date
-  var distance = countDownDate1 - now;
+  var distance = countDownDate2 - now;
     
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -17,12 +17,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("countdwn").innerHTML = days + "d " + hours + "h "
+  document.getElementById("count2").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdwn").innerHTML = "EXPIRED";
+    document.getElementById("count2").innerHTML = "EXPIRED";
   }
 }, 1000);
