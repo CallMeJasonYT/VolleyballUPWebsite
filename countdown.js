46c780c981ff91cfa796cb23e525ee4089c12b1d
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2023-05-03 18:59:39 +0300

    Update 03/05/2023

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 3, 2023 15:30:00").getTime();
+var countDownDate = new Date("May 4, 2023 20:00:00").getTime();
         
 // Update the count down every 1 second
 var x = setInterval(function() {
